ccfbe2a2f648461983399c84b7939549e0c59c1a
LauraCarniato <laura.salvatino@estudante.ifms.edu.br>
2026-05-14 09:59:55 -0400

    tentando fazer essa bosta funcionar

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package src;
+
+/**
+ *
+ * @author aluno
+ */
 public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        System.out.println("Hello, mundo!");
+        // TODO code application logic here
     }
+    
 }
